cb4c352071d5e11a9782745ebac20acbac752e7d
Peter <schmidt.multimedia@gmail.com>
2022-11-14 09:08:25 +0100

    module: addition a + b

--- script_01.js
+++ script_01.js
@@ -5,8 +5,16 @@
 1. Dateneingabe + -überprüfung :
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten :
-4. Ausgabe in Konsole :
+4. Ausgabe in Konsole : check!
 */
+
+// module: addition a + b |  test:
+// output(add(2,3));
+// output(add(2,-3));
+// output(add(2,0));
+function add(a,b) {
+   return a + b; 
+}
 
 // module: console output | test:
 // output("hello");
